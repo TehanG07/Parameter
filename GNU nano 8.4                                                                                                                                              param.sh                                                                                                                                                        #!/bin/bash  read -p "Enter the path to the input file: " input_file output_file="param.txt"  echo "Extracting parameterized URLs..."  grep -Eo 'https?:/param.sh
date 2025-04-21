52570c8774e5cdbd75fe01385793ad032a5ed057
TehanG07 <147998739+TehanG07@users.noreply.github.com>
2025-04-21 14:11:03 +0530

    Create param.sh

---
+++ GNU nano 8.4                                                                                                                                              param.sh                                                                                                                                                        #!/bin/bash  read -p "Enter the path to the input file: " input_file output_file="param.txt"  echo "Extracting parameterized URLs..."  grep -Eo 'https?:/param.sh
@@ -0,0 +1,9 @@
+read -p "Enter the path to the input file: " input_file
+output_file="param.txt"
+
+echo "Extracting parameterized URLs..."
+
+grep -Eo 'https?://[^ ]+\?[^ ]*' "$input_file" | grep '=' | sort -u > "$output_file"
+
+echo "Extraction complete. Results saved to $output_file"
+
